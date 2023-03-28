af955d7c542511893d40b2fd3edee2bb2ebd3bf1
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2023-03-27 18:20:58 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ function App() {
           <ProyectsIcon logo='mongodb' />
         </ProyectsLeft>
       </ProyectsHeader>
-      {/* coment */}
+
       <Contact
         title='Contact'
         titleInformation='Do you have a project in mind? Lets make it happen!'
